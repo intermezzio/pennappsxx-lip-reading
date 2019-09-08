c59e4e3c0f264dd8e4f46ae8e4363e8c7296d2f9
QuantumTCode <Neil.nitin.de@gmail.com>
2019-09-08 08:52:08 +0000

    Update Server

--- Server/Flask.py
+++ Server/Flask.py
@@ -69,8 +69,9 @@ def get_voice():
         text = textAndVoice[0]
         voices = textAndVoice[1]
         os.system("/home/ubuntu/anaconda3/envs/tensorflow_p36/bin/python3 ../ML-Audio/pennapp_audio.py --no_sound -n {} -t {}".format(voices,text))
+        response = make_response("../Server/demo_output_00.wav")
         response.headers['Content-Type'] = 'audio/wav'
-        response.headers['Content-Disposition'] = 'attachment; filename=voice.wav'
+        response.headers['Content-Disposition'] = 'attachment; filename=demo_output_00.wav'
         return response
     else:
         text_input = texttospeech.types.SynthesisInput(text=string)
